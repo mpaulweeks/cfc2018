dbdb8c1c1c47bf6e34b5b69dc6f69cfbb368a10b
mpaulweeks <mpaulweeks@gmail.com>
2018-04-17 14:58:44 -0400

    leftover from jquery

--- homework/canvas/example/main.js
+++ homework/canvas/example/main.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("#bigCanvas");
+var canvas = document.getElementById("bigCanvas");
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 var ctx = canvas.getContext('2d');
