d536c04884b8b676d8e16d5c45f1fd1467bbb62a
doritos123 <38043034+doritos123@users.noreply.github.com>
2018-04-17 18:51:19 -0400

    Add files via upload

---
+++ students/dorit/navbar/main.js
@@ -0,0 +1,6 @@
+// this snippet of javascript will execute code from comments.js
+// it will setup your form to submit correctly, and load previously submitted comments
+// make sure you have a form with id="comments-form" and a div with id="comments-view"
+
+// replace "example" with your name eg "paul"
+CFC.Comments.Setup("birds");
