e9e7b1368270172db74dfb9035413c96c8ff0433
mpaulweeks <mpaulweeks@gmail.com>
2018-05-08 12:45:51 -0400

    add random example

--- homework/mobile/example/main.js
+++ homework/mobile/example/main.js
@@ -1,4 +1,4 @@
-
+// advance text shown
 var textCount = 1;
 function nextText(){
   $("#text" + textCount).addClass("hidden");
@@ -11,3 +11,27 @@ function nextText(){
   $("#text" + textCount).removeClass("hidden");
 }
 $("#next").click(nextText);
+
+// randomly show element (in this case, numbers)
+var numberCount = 0;
+function randomNumber(){
+  $("#number" + numberCount).addClass("hidden");
+
+  // store the current number for comparison later
+  var oldCount = numberCount;
+
+  // generate random decimal, then force into a whole number
+  var randomDecimal = Math.random() * 10;
+  numberCount = Math.floor(randomDecimal);
+
+  // if our random pick was the same as the old pick, just go to the next one
+  if (oldCount == numberCount){
+    numberCount = numberCount + 1;
+    if (numberCount > 3){
+      numberCount = 1;
+    }
+  }
+
+  $("#number" + numberCount).removeClass("hidden");
+}
+$("#random").click(randomNumber);
